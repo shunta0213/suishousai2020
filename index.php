63cd373c6bc53d950907b355567aa0cdce203f08
shunta0213 <shunta16213@i.softbank.jp>
2020-01-29 23:06:13 +0900

    make subject
    Headerの横浜翠嵐づくり

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
   <meta charset="utf-8">
   <title>[公式]翠翔祭2020</title>
-  <style type="text/css" src="assets/main.css"></style>
-  <script type="text/javascript" src="assets/manu.css"></script>
+  <link rel="stylesheet" href="assets/main.css">
+  <script type="text/javascript" src="assets/menu.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-</head>
+  </head>
 <body>
-
+  <heaedr id="header">
+    <div class="name">YOKOHAMASUIRAN</div>
+  </heaedr>
 </body>
 </html>
