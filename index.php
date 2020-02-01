d3bfcd703b098cb9ee1fd7485b414fb9722f2543
shunta0213 <shunta16213@i.softbank.jp>
2020-02-01 09:29:30 +0900

    翠翔祭のロゴの一部

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
   <meta charset="utf-8">
   <title>[公式]翠翔祭2020</title>
   <link rel="stylesheet" href="assets/main.css">
-  <script type="text/javascript" src="assets/menu.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
 <body>
   <heaedr id="header">
-    <div class="name">YOKOHAMASUIRAN</div>
+    <div class="name">横浜翠嵐</div>
   </heaedr>
+  <section id="top">
+    <div　class="theme"><h1>翠翔祭</h1></div>
+  </section>
+
+  <script type="text/javascript" src="assets/menu.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
 </html>
