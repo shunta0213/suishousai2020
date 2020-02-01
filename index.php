a63cb83b8e766fd2aeb044813982f90d76493d40
shunta0213 <shunta16213@i.softbank.jp>
2020-02-01 20:40:48 +0900

    make top and post

--- index.php
+++ index.php
@@ -3,18 +3,32 @@
 <head>
   <meta charset="utf-8">
   <title>[公式]翠翔祭2020</title>
-  <link rel="stylesheet" href="assets/main.css">
+  <link rel="stylesheet" type="text/css" href="assets/main.css">
   <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
-  </head>
+</head>
 <body>
-  <heaedr id="header">
-    <div class="name">横浜翠嵐</div>
-  </heaedr>
-  <section id="top">
-    <div　class="theme"><h1>翠翔祭</h1></div>
-  </section>
-
-  <script type="text/javascript" src="assets/menu.js"></script>
+  <!-- 最初の画面 -->
+  <header id="header">
+    <div id="fixedheader">
+      <div class="name">横浜翠嵐</div>
+      <img href="assets/images.png" id="btn-menu">
+    </div>
+    <div id="top">
+      <div　id="theme">翠翔祭</div>
+    </div>
+  </header>
+  <!-- MAIN -->
+  <main>
+    <div class="timecountdown">
+      翠翔祭まで<div id="result"></div>
+    </div>
+    <div class="greet-main">
+    <h1 class="greet">ごあいさつ</h1>
+      翠翔祭ホームページへようこそ！
+    </div>
+  </main>
+  <script type="text/javascript" src="assets/js/menu.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/timecountdown.js"></script>
 </body>
 </html>
