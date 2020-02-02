7edb638f1f966927831a08f28ca36f35571d8227
shunta0213 <shunta16213@i.softbank.jp>
2020-02-02 21:33:19 +0900

    メニューを作った メディアクエリをPCファーストで実装

--- index.php
+++ index.php
@@ -3,18 +3,50 @@
 <head>
   <meta charset="utf-8">
   <title>[公式]翠翔祭2020</title>
-  <link rel="stylesheet" href="assets/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> <!-- for SmartPhone Mediaqueries  -->
+  <link rel="stylesheet" type="text/css" href="assets/main.css">
   <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
-  </head>
+</head>
 <body>
-  <heaedr id="header">
-    <div class="name">横浜翠嵐</div>
-  </heaedr>
-  <section id="top">
-    <div　class="theme"><h1>翠翔祭</h1></div>
-  </section>
-
-  <script type="text/javascript" src="assets/menu.js"></script>
+  <!-- 最初の画面 -->
+  <div id="top">
+    <header id="header">
+      <div id="fixedheader">
+        横浜翠嵐
+      </div>
+      <!-- menu -->
+      <a class="menu">
+          <span class="menu__line menu__line--top"></span>
+          <span class="menu__line menu__line--center"></span>
+          <span class="menu__line menu__line--bottom"></span>
+      </a>
+      <nav class="gnav">
+          <div class="gnav__wrap">
+              <ul class="gnav__menu">
+                  <li class="gnav__menu__item"><a href="">TOP</a></li>
+                  <li class="gnav__menu__item"><a href="">企画</a></li>
+                  <li class="gnav__menu__item"><a href="">パフォーマンス</a></li>
+              </ul>
+          </div><!--gnav-wrap-->
+      </nav>
+    </header>
+      <div id="theme">
+        翠翔祭
+      </div>
+    </div>
+  <!-- MAIN -->
+  <main>
+    <div class="timecountdown">
+      翠翔祭まで<div id="result"></div>
+    </div>
+    <div class="greet-main">
+    <h1 class="greet">ごあいさつ</h1>
+      翠翔祭ホームページへようこそ！
+    </div>
+  </main>
+  <script type="text/javascript" src="assets/js/menu.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/menu.js"></script>
+  <script type="text/javascript" src="assets/js/timecountdown.js"></script>
 </body>
 </html>
