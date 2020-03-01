1206cb96396fd91ffe32edfe4d77bbc3ac573a32
shunta0213 <shunta16213@i.softbank.jp>
2020-03-01 15:53:04 +0900

    テーマの反映、ビジュアルの微調整、整形

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,24 +1,24 @@
 // menu
-$('.menu').on('click',function(){
+$('.menu').on('click', function() {
     $('.menu__line').toggleClass('active');
     $('.gnav').fadeToggle();
 });
 
 //fade in
 var slideup = {
-  distance: '50px',
-  delay: '300',
-  easing: 'ease-in',
+    distance: '50px',
+    delay: '300',
+    easing: 'ease-in',
 }
 
 var slideup2 = {
-  distance: '50px',
-  delay: '600',
-  easing: 'ease-in',
+    distance: '50px',
+    delay: '600',
+    easing: 'ease-in',
 }
 
-$(function(){
-  ScrollReveal().reveal('.scrollreveal',slideup);
-  window.sr= new scrollReveal();
-  sr.reveal('.scrollreveal');
+$(function() {
+    ScrollReveal().reveal('.scrollreveal', slideup);
+    window.sr = new scrollReveal();
+    sr.reveal('.scrollreveal');
 })
